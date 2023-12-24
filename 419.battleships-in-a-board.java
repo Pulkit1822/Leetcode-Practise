8e9290db10635a1650deb692b47edc3552d48f73
Pulkit1822 <pulkitkumarmathur1@gmail.com>
2023-12-24 13:55:14 +0530

    Refactor removeShip method to remove print statements and improve readability

--- 419.battleships-in-a-board.java
+++ 419.battleships-in-a-board.java
@@ -21,38 +21,28 @@ class Solution {
         return count;
     }
     
-	// This is a helper class that overwrite the cell that has 'X' with '.'
-	// so that we won't see the battleship again.
+
     private int removeShip(char[][] board, int row, int col) {
-        // get our constrains 
+
         int maxR = board.length;
         int maxC = board[0].length;
-        
-        // if the cell is valid 
-        // i.e., it's within the boundary of the board
+
         if(row < maxR && col < maxC && row >= 0 && col >= 0) {
-            // Now we check to see if it's a ship
+
             if(board[row][col] == 'X') {
-			    System.out.print("[" + row + ", " + col + "] "); // Added print statement
-                // if the cell is 'X', then we set it to '.' 
-                // to indicate that we already saw it, and won't process it again
-                board[row][col] = '.'; // set it to empty
+			    System.out.print("[" + row + ", " + col + "] "); 
+                board[row][col] = '.'; 
                 
                 // check adjacent cells
-                removeShip(board, row, col - 1);    // left cell
-                removeShip(board, row, col + 1);    // right cell                
-                removeShip(board, row + 1, col);    // top cell
-                removeShip(board, row - 1, col);    // bottom cell
+                removeShip(board, row, col - 1);   
+                removeShip(board, row, col + 1);                   
+                removeShip(board, row + 1, col);    
+                removeShip(board, row - 1, col);   
 				
-				System.out.println(); // Added print statement
-				// Each ship's coordinate will be displayed in 1 line
-				
-                // once setting all cells to empty '.', we can return 1,
-                // as there are no adjacent battleships per the problem description
+				System.out.println(); 
                 return 1;
             }
         }        
-        // if it's out of bound, or not a battleShip spot
         return 0;        
     }
 }
