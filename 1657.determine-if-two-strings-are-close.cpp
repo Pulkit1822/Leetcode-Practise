6b807c8b14108da9ae49d6816c808f3e7fa385dd
Pulkit1822 <pulkitkumarmathur1@gmail.com>
2024-01-14 17:01:16 +0530

    Refactor variable names and improve code readability

---
+++ 1657.determine-if-two-strings-are-close.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=1657 lang=cpp
+ *
+ * [1657] Determine if Two Strings Are Close
+ */
+
+/**
+ * @brief Check if two strings are close.
+ * 
+ * @param word1 The first word.
+ * @param word2 The second word.
+ * @return True if the words are close, false otherwise.
+ */
+static bool closeStrings(const std::string& word1, const std::string& word2) {
+    return word1.size() == word2.size() && signature(word1) == signature(word2);
+}
+/**
+ * Calculates the signature of a word.
+ * 
+ * @param word The word to calculate the signature for.
+ * @return The signature of the word as an array of integers.
+ */
+static std::array<int, 26> signature(const std::string& word) {
+    std::array<int, 26> freq = {};
+    std::array<int, 26> ans = freq;
+    
+    for (char ch : word) {
+        ++freq[ch - 'a'];
+    }
+    
+    std::sort(std::rbegin(freq), std::rend(freq));
+    
+    for (int i = 0, j = 0; i < std::size(ans); ++i) {
+        if (ans[i]) {
+            ans[i] = freq[j++];
+        }
+    }
+    
+    return ans;
+}
+// @lc code=end
+
